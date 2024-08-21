8c8e0c2f5d5035b4942b2243ccbddf40720d61fd
sikatanju <saadtanju@gmail.com>
2024-08-21 08:42:32 +0600

    Added smaller version of the ship image for showing remaining lives

--- scoreboard.py
+++ scoreboard.py
@@ -67,6 +67,7 @@ class Scoreboard:
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
+            ship.image = pygame.image.load('images/alien_lives.bmp')
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
